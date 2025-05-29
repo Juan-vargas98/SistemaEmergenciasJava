c3faeab88529fae88a264db50b9585c4985ea054
Juan Ignacio Vargas Ortiz <jortizvargas1603@gmail.com>
2025-05-29 14:54:22 -0500

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("=== Sistema de Emergencias Iniciado ===");
-        new Operador().start();
-        new Despachador().start();
-    }
-}
